113b3db710aa119c0271324aadb355209f0b6817
sumon paul <sumpaul100@gmail.com>
2023-03-21 01:45:04 +0600

    http create and req, res

--- httpreq/http.js
+++ httpreq/http.js
@@ -1,7 +1,25 @@
 const http = require('http');
 
-const server = http.createServer(); // create a server
+const server = http.createServer((req, res)=>{
+    //it takes 2 paramiter
+  if(req.url === "/"){
+    res.write("hello programer!");
+    res.write("how are you doing");
+    res.end();
+  }else if(req.url === "/about"){
+    res.write("About us");
+    res.end();
+  }
+  else{
+    res.write("page not found");
+    res.end();
+  }
+    
+
+}); // create a server
 
 server.listen(3000) // server is event emitter
 
+
+//even loop stay always running
 console.log('Server listening on port 3000')// server in running 
