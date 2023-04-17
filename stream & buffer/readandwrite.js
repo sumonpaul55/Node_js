19352bec2dfa40766cbb39387df1ac50f199e03c
sumon paul <sumpaul100@gmail.com>
2023-04-18 01:56:45 +0600

    we can use pipe() for write file one from another

--- stream & buffer/readandwrite.js
+++ stream & buffer/readandwrite.js
@@ -4,7 +4,12 @@ const fs = require('fs')
 const ourreadStream = fs.createReadStream(`${__dirname}/bigdata.txt`)
 const writeStream = fs.createWriteStream(`${__dirname}/forwrite.txt`)
 
-ourreadStream.on("data", (chunk) => { 
-    writeStream.write(chunk); //data write in forwrite file from bigdata.txt
-    // console.log(chunk.toString()); //data read
-  });
+// ourreadStream.on("data", (chunk) => { 
+//     writeStream.write(chunk); //data write in forwrite file from bigdata.txt
+//     // console.log(chunk.toString()); //data read
+//   });
+
+// ************************ we can use it instead of avobe exmple ************************
+
+ourreadStream.pipe(writeStream)
+
